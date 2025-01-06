63a6a11360aa280200a865dcc8a54f10021d6084
zvenc <yaegereren835@gmail.com>
2025-01-06 23:51:54 +0100

    add tex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import fastGlob from 'fast-glob';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), fastGlob()],
+	integrations: [tailwind(), mdx()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 });
