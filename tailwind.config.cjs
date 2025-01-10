252d5435072dc3eede497e9fa5c458f86cbbcd40
zvenc <yaegereren835@gmail.com>
2025-01-10 01:02:41 +0100

    improve look and feel and add placeholders for upcoming posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,md,mdx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
